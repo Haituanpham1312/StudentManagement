7be0215febcef0cbd3d5fb004797e2df1b1202d1
Haituanpham <photoho1312@gmail.com>
2020-12-13 00:07:25 +0700

    delete

---
+++ deleteService.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sevice;
+
+import Entity.Student;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author AD
+ */
+public class deleteService {
+    
+}
